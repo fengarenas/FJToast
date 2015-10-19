d3285065443a71dd187cec7f80bbccd88cbe6edc
fengarenas <fengarenas@126.com>
2015-10-19 16:40:01 +0800

    Initial Commit

---
+++ FJToastDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FJToastDemo
+//
+//  Created by Fengj on 15/10/19.
+//  Copyright © 2015年 FengJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FJToastDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FJToastDemo
+//
+//  Created by Fengj on 15/10/19.
+//  Copyright © 2015年 FengJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
