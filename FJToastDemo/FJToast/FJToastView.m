669ab355eb112d88d0efbb4931ea2e6796950093
fengarenas <fengarenas@126.com>
2015-10-22 14:30:48 +0800

    cp3

--- FJToastDemo/FJToast/FJToastView.m
+++ FJToastDemo/FJToast/FJToastView.m
@@ -1,6 +1,7 @@
-//
-// Copyright (c) 2013 FJToast
-// Author:FengJun(http://devfeng.com/)
+// Copyright (c) 2015 FJToast
+// Author fengjun
+// Blog:http://devfeng.com/
+// Url :https://github.com/fengarenas/FJToast
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
